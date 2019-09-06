153b0fd71e1213d1477c701c9716901095dd0117
tomo1224410 <sadfgwqerty@gmail.com>
2019-09-06 22:20:40 +0900

    fix ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,37 +1,21 @@
  $(function(){ 
    function buildHTML(message){
-      if (message.image) {
-        var html = `<div class="chat-block">
-                      <div class="chat-block__name">
-                        ${message.user_name}
-                      </div>
-                      <div class="chat-block__time">
-                        ${message.date}
-                      </div>
-                    </div>
-                    <div class="chat-text">
-                      <p class="lower-message__content">
-                        ${message.content}
-                      </p>
-                    <img src=${message.image}>
-                    </div>`
-        return html;
-      } else {
-        var html = `<div class="chat-block">
-                      <div class="chat-block__name">
-                        ${message.user_name}
-                      </div>
-                      <div class="chat-block__time">
-                        ${message.date}
-                      </div>
-                    </div>
-                    <div class="chat-text">
-                      <p class="lower-message__content">
-                        ${message.content}
-                      </p>
-                    </div>`
-        return html;
-      };
+    var image = message.image ? `<img src=${message.image}>` :"";
+    var html = `<div class="chat-block">
+                  <div class="chat-block__name">
+                    ${message.user_name}
+                  </div>
+                  <div class="chat-block__time">
+                    ${message.date}
+                  </div>
+                </div>
+                <div class="chat-text">
+                  <p class="lower-message__content">
+                    ${message.content}
+                  </p>
+                  <img src=${message.image}>
+                </div>`
+    return html;
    }
   $(".new_message").on('submit', function(e){
       e.preventDefault();
