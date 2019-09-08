181b679ac35a380db6d49cf72bca2de88a252555
tomo1224410 <sadfgwqerty@gmail.com>
2019-09-08 18:47:26 +0900

    Incremental_search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.where('name LIKE(?)', "%#{params[:name]}%").limit(10)
+    respond_to do |format|
+      format.json
+    end
   end
 
   def edit
