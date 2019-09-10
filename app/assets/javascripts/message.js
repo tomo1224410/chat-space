cdf0d53c0e2a478e3d4445f3933d619682361249
tomo1224410 <sadfgwqerty@gmail.com>
2019-09-10 20:32:51 +0900

    autoreload

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){ 
   function buildHTML(message){
-  var image = message.image ? `<img src=${message.image}>` :"";
+  var image = message.image ? `<img src=${message.image} >` :"";
   var html = `<div class="chat" data-messageid="${message.id}">
                 <div class="chat-block">
                   <div class="chat-block__name">
@@ -43,18 +43,26 @@ $(function(){
       return false;
   });
   var reloadMessages = function() {
-    last_message_id = $('.chat:last').data('id');
-    $.ajax({
-      url: "/api/messages",
-      type: 'get',
-      dataType: 'json',
-      data: {id: last_message_id}
-    })
-    .done(function(messages) {
-      console.log('success');
-    })
-    .fail(function() {
-      console.log('error');
-    });
+    if(document.URL.match(/\/groups\/\d+\/messages/)) {
+      var last_message_id = $('.chat:last').data('messageid');
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function (messages) {
+        var insertHTML = '';
+        messages.forEach(function(message) {
+          insertHTML = buildHTML(message);
+          $('.chats').append(insertHTML);
+        })
+        $('.chats').animate({scrollTop: $('.chats')[0].scrollHeight}, 'fast');
+      })
+      .fail(function() {
+        alert('error');
+      });
+    }
   };
+  setInterval(reloadMessages, 5000);
 });
